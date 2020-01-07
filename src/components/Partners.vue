911e8227c49ed6ebb9736fad60a92f80e1e8bec8
Hold7 <lucasbchini@gmail.com>
2020-01-07 15:09:00 +0100

    2020

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -8,16 +8,16 @@
               alt="Logo Agriculture Biologique"
               src="../assets/logo_agri_bio.jpg"
               class="logo mx-2"
-            >
+            />
           </a>
           <a href="https://www.geoportail.gouv.fr/" target="_blank">
-            <img alt="IGN logo" src="../assets/ign.png" class="logo mx-2">
+            <img alt="IGN logo" src="../assets/ign.png" class="logo mx-2" />
           </a>
           <a
             href="https://entrepreneur-interet-general.etalab.gouv.fr/defis/2019/cartobio.html"
             target="_blank"
           >
-            <img alt="EIG logo" src="../assets/logo-eig2.png" class="logo mx-2">
+            <img alt="EIG logo" src="../assets/logo-eig2.png" class="logo mx-2" />
           </a>
           <a
             href="https://www.gouvernement.fr/secretariat-general-pour-l-investissement-sgpi"
@@ -27,12 +27,12 @@
               alt="Logo investir l'avenir"
               src="../assets/logo_investirlavenir.png"
               class="logo mx-2"
-            >
+            />
           </a>
           <v-divider vertical class="mx-3"></v-divider>
           <p class="madewith">Fait avec</p>
           <a href="https://vuetifyjs.com/en/" target="_blank">
-            <img alt="vuetify logo" src="../assets/logo.svg" class="logo mx-2">
+            <img alt="vuetify logo" src="../assets/logo.svg" class="logo mx-2" />
           </a>
         </v-layout>
       </v-card-text>
@@ -45,7 +45,7 @@
             </template>
             <Legal :dialog="dialog"></Legal>
           </v-dialog>
-          <v-divider vertical class="mx-3"></v-divider>&copy;2019 —
+          <v-divider vertical class="mx-3"></v-divider>&copy;2020 —
           <strong>Agence BIO</strong>
         </v-layout>
       </v-card-text>
