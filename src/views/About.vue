fb412e58d73be4c3e3da2301e051c1099e3d9ca9
Hold7 <lucasbchini@gmail.com>
2020-02-17 17:17:59 +0100

    updated about informations

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,122 +1,91 @@
 <template>
   <v-container grid-list-xl>
-    <h1>Qui sommes nous ?</h1>
-    <div
-      class="subheading text-xs-center mx-5 my-3"
-    >Nous sommes deux Entrepreneurs d'Intérêt Général, présents à l'Agence BIO pour 10 mois</div>
+    <h1>Vous êtes :</h1>
     <v-layout row wrap justify-center>
       <v-flex xs12 md4>
         <v-card class="elevation-0 transparent">
-          <v-avatar :size="100">
-            <img :src="require('../assets/wojtek-sitarz.png')" />
-          </v-avatar>
           <v-card-title primary-title class="layout justify-center">
             <div>
-              <div class="headline">Wojtek Sitarz</div>
-              <span class="grey--text">Géomaticien</span>
+              <div class="headline">Controleur Bio ?</div>
             </div>
           </v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn flat icon href="mailto:wojtek.sitarz@agencebio.org">
-              <v-icon class="bio-green">email</v-icon>
-            </v-btn>
-            <v-btn flat icon href="https://www.linkedin.com/in/wojtek-sitarz/" target="_blank">
-              <v-icon class="bio-green">fab fa-linkedin</v-icon>
-            </v-btn>
-            <v-btn flat icon href="https://github.com/wojteksitarz" target="_blank">
-              <v-icon class="bio-green">fab fa-github</v-icon>
-            </v-btn>
-            <v-btn
-              flat
-              icon
-              href="https://entrepreneur-interet-general.etalab.gouv.fr/communaute/2019/wojtek-sitarz.html"
+          <v-card-text>
+            Cette application vous est dédiée pour le moment. Vous pouvez vous connecter en utilisant vos identifiants de
+            <a
+              href="notification@agencebio.org"
               target="_blank"
-            >
-              <v-icon class="icon-eig">eig</v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-          </v-card-actions>
+            >notification@agencebio.org</a>
+            En cas de soucis, n'hésitez pas à nous contacter. Nous sommes toujours preneurs de retours et il est possible que nous vous contactions dans cet objectif.
+          </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12 md4 offset-md1>
-        <v-card class="elevation-0 transparent">
-          <v-avatar :size="100">
-            <img :src="require('../assets/lucas-bchini.png')" />
-          </v-avatar>
-          <v-card-title primary-title class="layout justify-center">
-            <div>
-              <div class="headline">Lucas Bchini</div>
-              <span class="grey--text">Développeur</span>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn flat icon href="mailto:lucas.bchini@agencebio.org">
-              <v-icon class="bio-green">email</v-icon>
-            </v-btn>
-            <v-btn
-              flat
-              icon
-              href="https://www.linkedin.com/in/lucas-bchini-9b1a09a8/"
-              target="_blank"
-            >
-              <v-icon class="bio-green">fab fa-linkedin</v-icon>
-            </v-btn>
-            <v-btn flat icon href="https://github.com/Hold7" target="_blank">
-              <v-icon class="bio-green">fab fa-github</v-icon>
-            </v-btn>
-            <v-btn
-              flat
-              icon
-              href="https://entrepreneur-interet-general.etalab.gouv.fr/communaute/2019/lucas-bchini.html"
-              target="_blank"
-            >
-              <v-icon class="icon-eig">eig</v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 md4 class="subheading text-xs-center my-3">
-        Nous sommes également accompagnés par nos mentors:
+      <v-flex xs12 md4>
         <v-card class="elevation-0 transparent">
           <v-card-title primary-title class="layout justify-center">
             <div>
-              <div class="headline">Samir Kebli</div>
-              <span class="grey--text">Responsable SI à l'Agence BIO</span>
+              <div class="headline">Agriculteur Bio ?</div>
             </div>
           </v-card-title>
+          <v-card-text>
+            Afin de mieux connaître les surfaces cultivées en agriculture biologique pour mesurer l'impact et mener des politiques publiques plus efficaces,
+            de fiabiliser le contrôle et de le rendre plus simple et plus rapide, CartoBio permet aux OC d'avoir accès en amont d'un contrôle aux données de votre déclaration PAC.
+            <br />Pour un agriculteur bio qui ne ferait pas de demande à la PAC, nous souhaitons également connaître la localisation de ses parcelles, afin d'avoir une meilleure connaissance du territoire.
+            Si vous avez des questions, des remarques, n'hésitez pas à nous contacter !
+          </v-card-text>
         </v-card>
+      </v-flex>
+      <v-flex xs12 md4>
         <v-card class="elevation-0 transparent">
           <v-card-title primary-title class="layout justify-center">
             <div>
-              <div class="headline">Dorian Fléchet</div>
-              <span class="grey--text">Chargé de l'observatoire national de l'agriculture biologique</span>
+              <div class="headline">Vous recherchez les données des parcelles Bio?</div>
             </div>
           </v-card-title>
+          <v-card-text>
+            Les données ne sont pas publiées en open data pour le moment. Si vous êtes une administration et que vous pouvez prétendre à l'obtention de celles-ci, nous vous invitons à nous contacter.
+            <br />Sinon, un peu de patience ! :)
+          </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12 md4 offset-md1 align-content-space-around class="mt-3">
-        <v-btn
-          outline
-          href="https://entrepreneur-interet-general.etalab.gouv.fr/"
-          target="_blank"
-        >En savoir plus sur le programme EIG</v-btn>
 
+      <v-flex xs12 md4 align-content-space-around class="mt-3">
         <v-card class="elevation-0 transparent mt-4">
           <v-card-title primary-title class="layout justify-center">
             <div>
-              <div>10 mois, c'est (déjà) fini ! Mais ne vous inquiêtez pas, si Wojtek quitte l'aventure, le projet continue ! Vous pouvez toujours nous contacter pour discuter du projet.</div>
-              <v-btn
-                round
-                color="primary-green"
-                class="mt-3"
-                dark
-                href="mailto:cartobio@beta.gouv.fr"
-              >Contactez nous!</v-btn>
+              <div class="headline">
+                Un projet
+                <a
+                  href="https://entrepreneur-interet-general.etalab.gouv.fr/"
+                  title="Site des Entrepreneurs d'Intérêt Général"
+                  target="_blank"
+                >EIG</a>...
+                <br />Devenu
+                <a
+                  href="https://beta.gouv.fr/"
+                  title="Site des Start Up d'Etat et de Territoires"
+                  target="_blank"
+                >Start Up D'Etat</a> !
+              </div>
             </div>
           </v-card-title>
+          <v-card-text>
+            A l'issue du programme Entrepreneurs d'Intérêt Général, nous nous sommes tournés vers beta.gouv afin de nous accompagner sur le déploiment du projet, et en assurer sa pérénnisation.
+            Plus d'information
+            <a
+              href="https://beta.gouv.fr/startups/cartobio.html"
+              title="Page de descriptif de CartoBio sur le site de beta.gouv"
+              target="_blank"
+            >ici</a>
+            <br />
+            <v-btn
+              round
+              title="Contactez nous"
+              color="primary-green"
+              class="mt-3"
+              dark
+              href="mailto:cartobio@beta.gouv.fr"
+            >Contactez nous!</v-btn>
+          </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
