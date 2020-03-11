d10b35317e64628b5817ddc20cfb554fc33807f2
Thomas Parisot <oncletom@users.noreply.github.com>
2020-03-11 11:02:50 +0100

    Track operators search
    Also, add form submission by pressing ENTER in any field

--- src/components/FilterToolbar.vue
+++ src/components/FilterToolbar.vue
@@ -1,42 +1,46 @@
 <template>
   <div d-inline-flex>
-    <v-layout>
-      <h3 class="mx-2 my-auto">Filtres:</h3>
-      <v-flex xs12 sm6 md3>
-        <v-text-field body-1 label="Nom" v-model="tempFilters.name" class="mx-2"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm6 md3>
-        <v-text-field label="PACAGE" v-model="tempFilters.pacage" class="mx-2"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm6 md3>
-        <v-text-field label="Numéro Bio" v-model="tempFilters.numeroBio" class="mx-2"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm6 md3>
-        <v-text-field label="Numéro Client" v-model="tempFilters.numeroClient" class="mx-2"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm6 md3>
-        <v-autocomplete
-          v-model="tempFilters.department"
-          label="Département"
-          :items="departmentList"
-          item-text="label"
-          item-value="id"
-          class="mx-2"
-        ></v-autocomplete>
-      </v-flex>
-      <v-flex xs12 sm6 md3>
-        <v-text-field label="Ville" v-model="tempFilters.city" class="mx-2"></v-text-field>
-      </v-flex>
-      <v-btn class="mx-2 my-auto" color="primary" @click="applyFilters()">Appliquer les filtres</v-btn>
-      <v-btn
-        class="mx-2 my-auto"
-        color="error"
-        @click="removeFilters()"
-        tooltip="Supprimer les filtres"
-      >
-        <v-icon>delete</v-icon>
-      </v-btn>
+    <form @submit="applyFilters()">
+      <v-layout>
+        <h3 class="mx-2 my-auto">Filtres:</h3>
+        <v-flex xs12 sm6 md3>
+          <v-text-field body-1 label="Nom" v-model="tempFilters.name" class="mx-2"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-text-field label="PACAGE" v-model="tempFilters.pacage" class="mx-2"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-text-field label="Numéro Bio" v-model="tempFilters.numeroBio" class="mx-2"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-text-field label="Numéro Client" v-model="tempFilters.numeroClient" class="mx-2"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-autocomplete
+            v-model="tempFilters.department"
+            label="Département"
+            :items="departmentList"
+            item-text="label"
+            item-value="id"
+            class="mx-2"
+          ></v-autocomplete>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-text-field label="Ville" v-model="tempFilters.city" class="mx-2"></v-text-field>
+        </v-flex>
+        <v-btn class="mx-2 my-auto" type="submit" color="primary" @click="applyFilters()">
+          <v-icon>search</v-icon> Appliquer les filtres
+        </v-btn>
+        <v-btn
+          class="mx-2 my-auto"
+          color="error"
+          @click="removeFilters()"
+          tooltip="Réinitialiser les filtres"
+        >
+          <v-icon>highlight_off</v-icon>
+        </v-btn>
     </v-layout>
+  </form>
   </div>
 </template>
 
@@ -68,6 +72,8 @@ export default {
   },
   methods: {
     applyFilters() {
+      // event: category, action, value
+      window._paq.push(['trackEvent', 'operators', 'search', JSON.stringify(this.tempFilters)]);
       this.$emit("update-filters", this.tempFilters);
       this.displayFiltersDialog = false;
     },
