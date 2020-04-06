a39419e73e18dd63d5a7d1bd7b12a08d12fab45a
lucas-bch <lucasbchini@gmail.com>
2020-04-06 18:15:40 +0200

    added dotenv to read env var in tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  verbose: true
 }
+
+// load env var to jest test
+require('dotenv').config({ path: '.env.local' });
