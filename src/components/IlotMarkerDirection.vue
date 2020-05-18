7c56c62dfc2ac7ca02ab8ee94a3413099879b2a3
lucas-bch <lucasbchini@gmail.com>
2020-05-18 17:18:42 +0200

    order and display of ilot direction improvements

--- src/components/IlotMarkerDirection.vue
+++ src/components/IlotMarkerDirection.vue
@@ -2,8 +2,10 @@
     <MglMarker :coordinates="hoveredIlotCoordinates" :draggable="false" :rotate="45" @added="onMarkerCreated">
         <div slot="marker" class="flex">
           <v-card color="transparent" flat :style="offsetStyle">
-            <v-chip>{{hoveredIlotName}}</v-chip>
-            <v-icon :style="angleRotationStyle">near_me</v-icon>
+            <v-layout :style="flexStyle" align-center>
+              <v-flex><v-chip>{{hoveredIlotName}}</v-chip></v-flex>
+              <v-flex><v-icon :style="angleRotationStyle">near_me</v-icon></v-flex>
+            </v-layout>
           </v-card>
         </div>
     </MglMarker>
@@ -25,16 +27,13 @@ export default {
     return {
       hoveredIlotCoordinates: [0,0],
       angleRotationStyle: '',
-      // Anchor options are 'center' , 'top' , 'bottom' , 'left' , 'right' , 'top-left' , 'top-right' , 'bottom-left' , and 'bottom-right'
-      // offset: [0,0],
-      offsetStyle: ''
+      offsetStyle: '',
+      flexStyle: ''
     }
   },
   methods: {
-    onMarkerCreated (e) {
+    onMarkerCreated () {
       this.showIlotLocation();
-      let marker = e.marker;
-      console.log(marker);
       // marker._offset =  this.offset;
       // marker._update();
     },
@@ -44,11 +43,12 @@ export default {
       let line = lineString([this.mapCenter, this.ilotCenterCoordinates]);
       let intersect = lineIntersect(line, mapOuterLine);
       this.hoveredIlotCoordinates = intersect.features[0].geometry.coordinates;
-      let {rotationAngle, offset} = this.getStyleNumbers(this.mapCenter, intersect.features[0].geometry.coordinates);
+      let {rotationAngle, offset, flex} = this.getStyleNumbers(this.mapCenter, intersect.features[0].geometry.coordinates);
       // let rotationAngle = this.getStyleNumbers(this.mapCenter, intersect.features[0].geometry.coordinates);
       this.angleRotationStyle = "transform : rotate(" + rotationAngle + "deg)";
       this.offsetStyle = "transform : translate(" + offset[0] + "px, " + offset[1] + "px);";
-      console.log(this.offsetStyle, rotationAngle);
+      this.flexStyle = "flex-direction : " + flex + ";";
+      console.log(this.offsetStyle, rotationAngle, this.flexStyle );
     },
     getStyleNumbers(mapCenter, intersectionPoint) {
       // calcAngle : StartPoint, Intersectionpoint, EndPoint
@@ -59,34 +59,40 @@ export default {
       let angleCorrection = -45; // default rotation of arrow icon = 45°
       let offset = [0,0];
       let explementary = false;
-      
-      if (intersectionPoint[0] === bounds._ne.lon) {
+      let flex = '';
+      if (intersectionPoint[0] === bounds._ne.lng) {
         corner = bounds._ne.toArray();
-        offset = [30, 0];
+        offset = [-60, 0];
+        angleCorrection += 180;
+        explementary = true;
+        flex = 'row';
       } else if (intersectionPoint[1] === bounds._ne.lat) {
         corner = bounds._ne.toArray();
-        offset = [0, 30];
+        offset = [0, 40];
         angleCorrection -= 90;
+        flex = 'column-reverse';
         if (intersectionPoint[0] > mapCenter[0]) {
           explementary = true;
         } else {
           explementary = false;
         }
-      } else if (intersectionPoint[0] === bounds._sw.lon) {
-        offset = [-30, 0];
+      } else if (intersectionPoint[0] === bounds._sw.lng) {
+        offset = [60, 0];
         corner = bounds._sw.toArray();
-        angleCorrection += 180;
+        flex = 'row-reverse';
       } else if (intersectionPoint[1] === bounds._sw.lat) {
-        offset = [0, -30];
+        offset = [0, -40];
         corner = bounds._sw.toArray();
         angleCorrection += 90;
+        flex = 'column';
       } else {
         corner = [0,0];
       }
       
       return {
         rotationAngle : calcAngle(mapCenter, intersectionPoint, corner, {explementary: explementary}) + angleCorrection,
-        offset : offset
+        offset : offset,
+        flex: flex
       };
     }
   }
