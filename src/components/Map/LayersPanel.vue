21a8adb7da7603f72ad01ab150e56b49b46d51ef
lucas-bch <lucasbchini@gmail.com>
2020-06-24 11:42:35 +0200

    layers panel in separated component

---
+++ src/components/Map/LayersPanel.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-expansion-panel popout expand v-bind:value="expandLayers">
+    <v-expansion-panel-content>
+      <template v-slot:header>
+        <div class="expansion-title">
+          <v-icon class="mr-2">layers</v-icon>Calques
+        </div>
+      </template>
+      <v-card>
+        <!-- <v-list dense class="pt-0" v-if="operator.title">
+            <v-list-tile subheader>
+              <v-subheader>{{ operator.title }}</v-subheader>
+            </v-list-tile>
+        List of years with parcels from the operator-->
+        <!--<v-list-tile
+              v-for="(year, index) in sortedYears"
+              :key="index"
+              @click="toggleLayerOperator(year)"
+            >
+              <v-list-tile-action>
+                <v-switch
+                  :color="layersVisible[year].colorBio"
+                  v-model="layersVisible[year].visibility"
+                  @click="toggleLayerOperator(year)"
+                />
+              </v-list-tile-action>
+              <v-list-tile-content>{{year}}</v-list-tile-content>
+            </v-list-tile>
+        </v-list>-->
+
+        <v-divider></v-divider>
+
+        <!-- <v-list-tile>
+            <v-subheader>Registre Parcellaire Graphique Bio</v-subheader>
+        </v-list-tile>-->
+
+        <!-- <v-list class="pt-0" dense>
+            List of years with parcels from the operator
+            <v-list-tile
+              v-for="(year, index) in sortedYears"
+              :key="index"
+              @click="toggleLayerAnon(year)"
+            >
+              <v-list-tile-action>
+                <v-switch
+                  :color="layersVisible['anon' + year].color"
+                  v-model="layersVisible['anon' + year].visibility"
+                  @click="toggleLayerAnon(year)"
+                ></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-content>{{year}}</v-list-tile-content>
+            </v-list-tile>
+        </v-list>-->
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+export default {
+  name: "LayersPanel",
+  props: {
+    operator: Object
+  },
+  data() {
+    return {
+      expandLayers: 0
+    };
+  },
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -74,60 +74,8 @@
         </MglMap>
 
         <!-- Layers selector -->
-        <v-flex class="layers-panel" v-show="isAuthenticated">
-          <v-expansion-panel popout expand v-bind:value="expandLayers">
-            <v-expansion-panel-content>
-              <template v-slot:header>
-                <div class="expansion-title">
-                  <v-icon class="mr-2">layers</v-icon> Calques
-                </div>
-              </template>
-              <v-card>
-                <v-list dense class="pt-0" v-if="operator.title">
-                  <v-list-tile subheader>
-                    <v-subheader>{{ operator.title }}</v-subheader>
-                  </v-list-tile>
-                  <!-- List of years with parcels from the operator -->
-                  <v-list-tile
-                    v-for="(year, index) in sortedYears"
-                    :key="index"
-                     @click="toggleLayerOperator(year)"
-                  >
-                    <v-list-tile-action>
-                      <v-switch :color="layersVisible[year].colorBio" v-model="layersVisible[year].visibility" @click="toggleLayerOperator(year)" />
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                      {{year}}
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </v-list>
-
-                <v-divider></v-divider>
-
-                <v-list-tile>
-                  <v-subheader>Registre Parcellaire Graphique Bio</v-subheader>
-                </v-list-tile>
-
-                <v-list class="pt-0" dense>
-                  <!-- List of years with parcels from the operator -->
-                  <v-list-tile
-                    v-for="(year, index) in sortedYears"
-                    :key="index"
-                    @click="toggleLayerAnon(year)"
-                  >
-                    <v-list-tile-action>
-                      <v-switch :color="layersVisible['anon' + year].color" v-model="layersVisible['anon' + year].visibility" @click="toggleLayerAnon(year)"></v-switch>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                      {{year}}
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </v-list>
-              </v-card>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-flex>
-
+        <LayersPanel v-show="isAuthenticated"></LayersPanel>
+        
       </div>
     </v-content>
   </v-layout>
@@ -158,6 +106,7 @@ import ParcelDetails from "@/components/ParcelDetails";
 import ParcelDetailsPopup from "@/components/ParcelDetailsPopup";
 import SearchSidebar from "@/components/Map/SearchSidebar";
 import IlotMarkerDirection from "@/components/IlotMarkerDirection";
+import LayersPanel from "@/components/Map/LayersPanel";
 
 import { mapGetters, mapState } from 'vuex';
 
@@ -218,6 +167,7 @@ export default {
     MglScaleControl,
     MglMap,
     IlotMarkerDirection,
+    LayersPanel,
     MglGeojsonLayer
   },
   data() {
