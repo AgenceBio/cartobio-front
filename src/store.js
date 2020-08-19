7c0059425572fb4f09d6597144c048396fbe8133
lucas-bch <lucasbchini@gmail.com>
2020-08-19 16:54:19 +0200

    store with exploitation view

--- src/store.js
+++ src/store.js
@@ -17,6 +17,7 @@ const store =  new Vuex.Store({
         operators,
         pacage,
         user,
+        exploitationView
     },
     state: {
         lastDataUpdate: '2019-05-15',
@@ -54,6 +55,7 @@ const store =  new Vuex.Store({
                     break;
                 case "Admin":
                     userCat = this.state.categories.admin;
+                    break;
             }
             state.userCategory = userCat;
         },
