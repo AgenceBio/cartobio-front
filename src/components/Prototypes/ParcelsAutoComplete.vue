a654ecfd4f7bbaa831e772e9c0c1b267b12f13a1
Thomas Parisot <oncletom@users.noreply.github.com>
2021-02-24 11:05:47 +0100

    Grid view, and display map on interaction

--- src/components/Prototypes/ParcelsAutoComplete.vue
+++ src/components/Prototypes/ParcelsAutoComplete.vue
@@ -6,90 +6,96 @@
     <p>
       En renseignant votre parcellaire vous participez à faciliter votre contrôle AB et l’instruction de vos aides PAC si vous les demandez.<br/>
 Les données renseignées seront uniquement communiquées à votre Organisme Certificateur en l’état. Vos données anonymisées permettront également d’améliorer
- la connaissance des parcelles bio en France. 
+ la connaissance des parcelles bio en France.
 
     </p>
     <h2>Saisie du parcellaire</h2>
 
+    <div class="grid">
+      <Fragment>
+        <span class="header">Commune</span>
+        <span class="header">Parcelles cadastrales</span>
+        <span class="header">Type de culture</span>
+        <span class="header">Statut de conversion</span>
+        <span class="header">Date de conversion</span>
+        <span class="header">Commentaire</span>
+        <span class="header"></span>
+      </Fragment>
+      <Fragment class="row xs12 d-flex" v-for="(plot, index) in plots" :key="index">
+        <v-autocomplete single-line outline v-model="plot.com" :item-text="itemText" item-value="COM" :items="_communes" :hide-details="index !== plots.length - 1" />
+        <v-text-field single-line hint="Sous la forme AZ01, AN5, 011K0038 etc." persistent-hint clearable outline v-model="plot.cadastre_suffixes" :hide-details="index !== plots.length - 1" />
+        <v-autocomplete single-line outline :items="knownCultures" item-text="Libellé Culture" item-value="Code Culture" multiple v-model="plot.culture_type" :hide-details="index !== plots.length - 1" />
+        <v-select single-line outline v-model="plot.niveau_conversion" :items="conversion_levels" :hide-details="index !== plots.length - 1" />
+        <v-text-field single-line clearable outline mask="##/##/####" hint="Format Jour/Mois/Année. Si inconnue, donner la date de conversion prévue" persistent-hint :disabled="!plot.niveau_conversion || plot.niveau_conversion === 'CONV'" v-model="plot.engagement_date"  :hide-details="index !== plots.length - 1" />
+        <v-text-field single-line hint="Nom de la parcelle, précisions, autres infos ..." persistent-hint clearable outline v-model="plot.comment" :hide-details="index !== plots.length - 1" />
+        <v-btn flat icon large @click="$delete(plots, index)"><v-icon medium>delete</v-icon></v-btn>
+      </Fragment>
+    </div>
 
-    <v-flex class="row xs12 d-flex" v-for="(plot, index) in plots" :key="index">
-      <v-autocomplete label="Commune" hint="Nom de la commune (Code INSEE)" persistent-hint clearable outline v-model="plot.com" :item-text="itemText" item-value="com" :items="_communes" />
-      <v-text-field label="Numéro cadastral" hint="Sous la forme AZ01, AN5, 011K0038 etc." persistent-hint clearable outline v-model="plot.cadastre_suffixes" />
-      <v-autocomplete label="Type de culture" outline :items="knownCultures" item-text="Libellé Culture" item-value="Code Culture" multiple v-model="plot.culture_type" />
-      <v-select label="Statut conversion" outline v-model="plot.niveau_conversion" :items="conversion_levels" />
-      <v-menu v-model="plot.conversionDateMenu" lazy transition="scale-transition" offset-y full-width max-width="320px">
-        <template v-slot:activator="{ on }">
-          <v-text-field outline label="Date de conversion" v-on="on" hint="Si inconnue, donner la date de conversion prévue" persistent-hint readonly :disabled="!plot.niveau_conversion || plot.niveau_conversion === 'CONV'" v-model="plot.engagement_date" />
-        </template>
-
-        <v-date-picker outline @input="plot.conversionDateMenu = false" type="month" v-model="plot.engagement_date" show-current locale="fr-FR" />
-      </v-menu>
-      <v-text-field label="Commentaire" hint="Nom de la parcelle, précisions, autres infos ..." persistent-hint clearable outline v-model="plot.comment" />
-      <v-btn flat icon large @click="$delete(plots, index)"><v-icon large>delete</v-icon></v-btn> 
-    </v-flex>
-
-    <v-btn color="info" @click="addPlot">Ajouter une parcelle</v-btn>
+    <v-btn color="info" @click="addPlot() || fetchCadastreSheets()">
+      <v-icon class="mr-2">add_circle_outline</v-icon>
+      Ajouter une parcelle
+    </v-btn>
 
     <hr class="my-4" />
-    <h2>Visualisation du parcellaire <v-btn color="info" @click.stop="fetchCadastreSheets" :loading="loading" small>calculer les surfaces</v-btn></h2>
-    <v-expansion-panel expand :value="[true, true]">
-      <v-expansion-panel-content>
-        <template v-slot:header>
-          <h2>Vue tabulaire </h2>
-        </template>
 
-        <table class="summary">
-          <thead>
-            <tr>
-              <td>Parcelle</td>
-              <td>Production végétale</td>
-              <td>Date engagement</td>
-              <td>Réf cadastrale</td>
-              <td>Classement</td>
-              <td>Surface</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="plot in structuredPlots" :key="plot.plot_id + plot.cadastre_id">
-              <td>{{ plot.plot_id }}</td>
-              <td>{{ plot.culture_type }}</td>
-              <td>{{ plot.engagement_date }}</td>
-              <td><a :href="'https://cadastre.data.gouv.fr/map?style=ortho&amp;parcelleId=' + plot.cadastre_id" target="cadastre">{{ plot.cadastre_id }}</a></td>
-              <td>{{ plot.niveau_conversion }}</td>
-              <td>{{ plot.surface === null ? '?' : `${plot.surface}ha`}}</td>
-            </tr>
-          </tbody>
-        </table>
-      </v-expansion-panel-content>
-      
-      <v-expansion-panel-content>
-      <template v-slot:header>
-          <h2>Vue Map</h2>
-        </template>
-        <div class="map">
-          <MglMap
-          :mapStyle="mapStyle"
-          :bounds.sync="mapBounds"
-          @load="onMapLoaded"
-          ref="mapboxDiv"
-          >
-          <MglGeojsonLayer
-            sourceId="plots"
-            :layer="layerStyle('operator-parcels')"
-            layerId="operator-parcels"/>
-          <MglVectorLayer
-              before="place-continent"
-              sourceId="cadastre"
-              :layer="layerStyle('selectable-cadastral-parcels')"
-              layerId="parcelles"/>
-          </MglMap>
-        </div>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+    <v-btn color="info" @click.stop="fetchCadastreSheets" :loading="loading">
+      <v-icon class="mr-2">map</v-icon>
+      {{isMapVisible ? 'actualiser la carte' : 'afficher sur une carte'}}
+    </v-btn>
+
+    <section v-if="isMapVisible">
+      <h2 class="headline my-3">Parcellaire tabulaire </h2>
+
+      <table class="summary">
+        <thead>
+          <tr>
+            <td>Parcelle</td>
+            <td>Production végétale</td>
+            <td>Date engagement</td>
+            <td>Réf cadastrale</td>
+            <td>Classement</td>
+            <td>Surface</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="plot in structuredPlots" :key="plot.plot_id + plot.cadastre_id">
+            <td>{{ plot.plot_id }}</td>
+            <td>{{ plot.culture_type }}</td>
+            <td>{{ plot.engagement_date }}</td>
+            <td><a :href="'https://cadastre.data.gouv.fr/map?style=ortho&amp;parcelleId=' + plot.cadastre_id" target="cadastre">{{ plot.cadastre_id }}</a></td>
+            <td>{{ plot.niveau_conversion }}</td>
+            <td>{{ plot.surface === null ? '?' : `${plot.surface}ha`}}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2 class="headline my-3">Parcellaire graphique</h2>
+
+      <div class="map">
+        <MglMap
+        :mapStyle="mapStyle"
+        :bounds.sync="mapBounds"
+        @load="onMapLoaded"
+        ref="mapboxDiv"
+        >
+        <MglGeojsonLayer
+          sourceId="plots"
+          :layer="layerStyle('operator-parcels')"
+          layerId="operator-parcels"/>
+        <MglVectorLayer
+            before="place-continent"
+            sourceId="cadastre"
+            :layer="layerStyle('selectable-cadastral-parcels')"
+            layerId="parcelles"/>
+        </MglMap>
+      </div>
+    </section>
   </v-form>
 </template>
 
 <script>
+import {Fragment} from 'vue-fragment'
 import {get} from 'axios'
 import communes from '@/api/insee/communes.json'
 import {codes} from '@/modules/codes-cultures/pac.js'
@@ -115,8 +121,9 @@ const IN_HECTARES = 10000
 
 function prepareRow (row, cadastre_plots = {}) {
   const {culture_type, com, engagement_date, niveau_conversion} = row
-  
 
+  const [day, month, year] = engagement_date.split('/')
+  const formatted_engagement_date = `${year}-${month}-${day}`
 
   // 26108000AN0100
   const [, prefixe, section, parcelle] = row.cadastre_suffix.trim().match(/^(\d{0,3})([a-zA-Z]{1,2})(\d+)$/) ?? []
@@ -134,7 +141,7 @@ function prepareRow (row, cadastre_plots = {}) {
   return {
     com,
     culture_type,
-    engagement_date,
+    engagement_date: formatted_engagement_date,
     cadastre_id,
     plot_id,
     niveau_conversion,
@@ -143,6 +150,13 @@ function prepareRow (row, cadastre_plots = {}) {
 }
 
 export default {
+  components: {
+    MglMap,
+    MglGeojsonLayer,
+    MglVectorLayer,
+    Fragment
+  },
+
   computed: {
     structuredPlots () {
       const plots = []
@@ -169,23 +183,23 @@ export default {
     }
   },
 
-  components: {
-    MglMap,
-    MglGeojsonLayer,
-    MglVectorLayer
-  },
-
   data () {
     return {
       valid: false,
       loading: false,
+      isMapVisible: false,
 
       conversion_levels: [
+        { value: "Cx", text: "En attente du 1er contrôle AB" },
         { value: "C1", text: "C1" },
         { value: "C2", text: "C2" },
         { value: "C3", text: "C3" },
         { value: "BIO", text: "Bio" },
         { value: "CONV", text: "Conventionnel" },
+        { value: "HVE3", text: "Haute Valeur Environnementale (niveau 3)" },
+        { value: "HVEx", text: "Haute Valeur Environnementale (niveau 1 ou 2)" },
+        { value: "DMTR", text: "Demeter" },
+        { value: "N&P", text: "Nature & Progrès" },
         { value: "Inconnu", text: "Inconnu" }
       ],
 
@@ -197,14 +211,14 @@ export default {
           "cadastre_suffixes": 'ZI631, ZI637',
           "culture_type": ['AIL'],
           "niveau_conversion": 'BIO',
-          "engagement_date": "2017-02-03"
+          "engagement_date": "03/02/2017"
         },
         {
           "com": "26108",
           "cadastre_suffixes": 'AM17',
           "culture_type": ['SOJ'],
           "niveau_conversion": 'C2',
-          "engagement_date": "2017-02-03"
+          "engagement_date": "03/02/2017"
         }
       ],
 
@@ -237,9 +251,9 @@ export default {
   methods: {
     addPlot () {
       const lastLine = this.plots[ this.plots.length - 1 ] ?? {}
-      const { com, engagement_date } = lastLine
+      const { com, engagement_date, niveau_conversion } = lastLine
 
-      this.plots.push({ com, engagement_date })
+      this.plots.push({ com, engagement_date, niveau_conversion })
     },
 
     fetchCadastreSheets () {
@@ -264,7 +278,10 @@ export default {
           }))
       })
 
-      Promise.all(callsP).finally(() => this.loading = false)
+      Promise.all(callsP).finally(() => {
+        this.loading = false
+        this.isMapVisible = true
+      })
     },
 
     /**
@@ -275,8 +292,8 @@ export default {
       return cartobioStyle.layers.find(({ id }) => id === styleId);
     },
 
-    itemText ({ com, libelle }) {
-      return `${libelle} (${com})`
+    itemText ({ LIBELLE, DEP }) {
+      return `${LIBELLE} (${DEP})`
     },
 
     onMapLoaded({ map }) {
@@ -321,10 +338,22 @@ export default {
   h2 {
     margin-top : 0;
   }
-  .map { 
-    position: relative; 
+  .map {
+    position: relative;
     width: 100%;
     height: 500px;
   }
-  
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-column-gap: 5px;
+    grid-row-gap: 1em;
+    margin: 1em 0;
+
+    span.header {
+      font-weight: bold;
+    }
+  }
+
 </style>
