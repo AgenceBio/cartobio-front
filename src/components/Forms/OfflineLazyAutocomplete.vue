ef67f590295c056ca9ee711b5e88d224549c9d21
Thomas Parisot <oncletom@users.noreply.github.com>
2021-03-10 21:16:20 +0100

    Autocomplete uses native HTML5 search field

--- src/components/Forms/OfflineLazyAutocomplete.vue
+++ src/components/Forms/OfflineLazyAutocomplete.vue
@@ -1,8 +1,18 @@
+<template>
+  <Fragment>
+    <input type="search" ref="input" :list="listId" v-model="search" @input="handleInputChange" autocomplete="off" />
+    <datalist :id="listId">
+      <option v-for="item in items" :key="getValue(item)+getText(item)" :value="getValue(item)">{{ getText(item) }}</option>
+    </datalist>
+  </Fragment>
+</template>
 <script>
-import VAutoComplete from 'vuetify/lib/components/VAutocomplete'
+import {Fragment} from 'vue-fragment'
 
-export default VAutoComplete.extend({
-  inheritAttrs: true,
+export default {
+  components: {
+    Fragment
+  },
 
   props: {
     appendIcon: {
@@ -13,43 +23,105 @@ export default VAutoComplete.extend({
     },
     openOnClear: {
       default: true
+    },
+    lastItem: {
+      type: Number,
+      default: 20
+    },
+    itemText: {
+      default: 'text'
+    },
+    itemValue: {
+      default: 'value'
     }
   },
 
   data () {
     return {
-      search: null
+      items: [],
+      listId: 'list-' + (Math.random() * 1000000),
+      selectedItem: null,
+      search: ''
     }
   },
 
   created () {
-    this.$on('update:searchInput', (value) => this.search = value)
+    if (this.$attrs.value) {
+      this.selectItem(this.$attrs.value)
+    }
+
+    // this.$on('update:searchInput', (value) => this.search = value)
   },
 
-  beforeUpdate () {
-    if (this.value !== undefined) {
-      this.cachedItems = this.searchFor({ val: this.value, accessor: this.getValue })
-      this.setSelectedItems()
+  // beforeUpdate () {
+  //   if (this.value !== undefined) {
+  //     this.items = this.searchFor({ val: this.value, accessor: this.getValue })
+  //   }
+  // },
+
+  computed: {
+    textValue () {
+      return this.selectedItem ? this.getText(this.selectedItem) : this.value
     }
   },
 
   methods: {
+    getValue (item) {
+      const {itemValue} = this
+      return typeof itemValue === 'function' ? itemValue(item) : item[itemValue]
+    },
+
+    getText (item) {
+      const {itemText} = this
+      return typeof itemText === 'function' ? itemText(item) : item[itemText]
+    },
+
+    handleInputChange ($event) {
+      const val = $event.target.value
+
+      // listen to datalist selection
+      if ($event.inputType === 'insertReplacementText') {
+        this.selectItem(val)
+        return
+      }
+
+      // perform a search
+      if (val && val.length > 2) {
+        this.items = this.searchFor({ val, accessor: this.getText })
+      }
+
+      this.$nextTick(() => {
+        this.$refs.input.focus()
+      })
+    },
+
+    selectItem (val) {
+      this.selectedItem = this.searchOne({ val, accessor: this.getValue })
+      this.search = this.getText(this.selectedItem)
+    },
+
     searchFor ({ val, accessor }) {
+      if (val === undefined) {
+        return []
+      }
+
       const searches = (Array.isArray(val) ? val : [val]).map(v => v.toLocaleLowerCase())
       const entries = this.$attrs['lazy-items']()
 
       return entries
         .filter(item => searches.find(search => accessor(item).toLocaleLowerCase().includes(search)))
         .slice(0, this.lastItem)
+    },
+
+    searchOne ({ val, accessor }) {
+      return this.$attrs['lazy-items']().find((item => accessor(item) === val))
     }
   },
 
   watch: {
-    search (val) {
-      if (val && val.length > 2) {
-        this.cachedItems = this.searchFor({ val, accessor: this.getText })
-      }
+    selectedItem (item) {
+      this.$emit('input', this.getValue(item))
     }
   }
-})
+}
 </script>
