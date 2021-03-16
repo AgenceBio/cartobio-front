77a94905ea9474b3f44995ac2e743fdc6e2d88c2
Thomas Parisot <oncletom@users.noreply.github.com>
2021-03-16 19:25:37 +0100

    fix: make them work for Alsace/Moselle

--- src/cadastre.js
+++ src/cadastre.js
@@ -1,9 +1,12 @@
-const REFERENCE_RE = /((000)?\s*([a-z]{1,2})\s*([0-9]{1,4}))(?:\W|$)/ig
+const REFERENCE_RE = /(?=\W|^)((000|\d{3})?\s*([a-z]{1,2})\s*([0-9]{1,4}))(?=\W|$)/ig
+const ALSACE_MOSELLE_RE = /(?=\W|^)((000|\d{3})?\s*([0-9]{0,2}[1-9]{1})\s*([0-9]{1,4}))(?=\W|$)/ig
 
 export function parseReferences(string, { com }) {
-  const references = Array.from(string.matchAll(REFERENCE_RE)).map(([,, prefixe, section, parcelle]) => {
+  const RE = ['57', '67', '68'].includes(com.slice(0, 2)) ? ALSACE_MOSELLE_RE : REFERENCE_RE
+
+  const references = Array.from(string.matchAll(RE)).map(([,, prefixe, section, parcelle]) => {
     if (section) {
-      return `${com}${prefixe || '000'}${section}${parcelle.padStart(4, 0)}`
+      return `${com}${prefixe || '000'}${RE === REFERENCE_RE ? section : section.padStart(2, 0)}${parcelle.padStart(4, 0)}`
     }
   })
 
