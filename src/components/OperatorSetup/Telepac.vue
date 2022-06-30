46388510f17d522ac9b486052f265ea370fd45c0
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-06-30 15:01:36 +0200

    Rewrite Telepac help text thanks to @juliette-pal

--- src/components/OperatorSetup/Telepac.vue
+++ src/components/OperatorSetup/Telepac.vue
@@ -8,28 +8,54 @@
       </button>
     </div>
 
+    <fieldset class="fr-fieldset fr-my-5w">
+      <legend class="fr-fieldset__legend fr-icon fr-icon-questionnaire-line">
+        Où trouver mon fichier ?
+      </legend>
 
-    <details class="fr-my-5w">
-      <summary class="fr-icon-questionnaire-line fr-btn fr-btn--secondary fr-btn--icon-left">Où trouver mon fichier ?
-      </summary>
+      <div class="fr-fieldset__content">
+        <p>
+          Le fichier <b>Fichier de parcelles déclarées {{ campagnePacAnnee }}</b> se récupère en quelques clics
+          sur le <a href="https://www.telepac.agriculture.gouv.fr/" target="_blank">portail Telepac</a> :
+        </p>
 
-      <p>
-        Le fichier <b>Parcelles déclarées {{ campagnePacAnnee }} › Fichier de parcelles</b> se trouve sur le <a
-          href="https://www.telepac.agriculture.gouv.fr/" target="_blank">portail Telepac</a>,
-        dans la <a :href="campagnePacUrl" target="_blank">téléprocédure <b>Dossier PAC {{ campagnePacAnnee }}</b></a>,
-        dans l'onglet bleu "Import/export" puis <a :href="campagnePacExportUrl" target="_blank">Export îlots et
-          parcelles</a>.
-      </p>
-      <p>
-        <img src="/import/telepac-export.png" class="screenshot"
-          alt="Écran Import/Export du dossier PAC sur le service en ligne Telepac" />
-      </p>
-    </details>
+        <ol class="fr-list">
+          <li>
+            connectez-vous avec vos identifiants Télépac
+          </li>
+
+          <li>
+            sélectionnez l'entrée
+            <a :href="campagnePacUrl" class="fr-text--bold" target="_blank">Dossier PAC {{ campagnePacAnnee }}</a>
+            dans l'encart <b>Téléprocédures</b>
+
+            <img src="/import/capture_ecran_telepac_accueil.png" class="fr-responsive-img fr-ratio-4x3 screenshot"
+            alt="Écran Import/Export du dossier PAC sur le service en ligne Telepac" />
+          </li>
+
+          <li>
+            sélectionnez le menu
+            <a :href="campagnePacExportUrl" class="fr-text--bold" target="_blank">Export îlots et parcelles</a>
+            dans l'onglet bleu <b>Import/export</b>
+
+            <img src="/import/capture_ecran_telepac_export.png" class="fr-responsive-img fr-ratio-4x3 screenshot"
+            alt="Écran Import/Export du dossier PAC sur le service en ligne Telepac" />
+          </li>
+
+          <li>
+            cliquez sur le lien
+            <a :href="campagnePacExportShapefileUrl" target="_blank">
+              <b>Parcelles déclarées {{ campagnePacAnnee }}</b> : Fichier de parcelles
+            </a>
+          </li>
+        </ol>
+      </div>
+    </fieldset>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, toRef } from 'vue'
+import { ref, computed, readonly, toRef } from 'vue'
 import { post } from 'axios'
 import { statsPush } from '../../pages/stats.js'
 import store from '../../store.js'
@@ -43,9 +69,21 @@ const campagnePacAnnee = ref(2022)
 const campagnePacAnneeShort = computed(() => String(campagnePacAnnee.value).slice(-2))
 const campagnePacUrl = computed(() => `https://www.telepac.agriculture.gouv.fr/telepac/tas${campagnePacAnneeShort.value}/auth/accueilTas.action?campagne=${campagnePacAnnee.value}&titreApplication=Dossier+PAC+${campagnePacAnnee.value}`)
 const campagnePacExportUrl = computed(() => `https://www.telepac.agriculture.gouv.fr/telepac/tas${campagnePacAnneeShort.value}/ie/exportShpIlots.action`)
+const campagnePacExportShapefileUrl = computed(() => `https://www.telepac.agriculture.gouv.fr/telepac/tas${campagnePacAnneeShort.value}/ie/exportShpFichierParcelles.action?anneeCampagne=${campagnePacAnnee.value}`)
+const campagnePacExportXmlUrl = computed(() => `https://www.telepac.agriculture.gouv.fr/telepac/tas${campagnePacAnneeShort.value}/ie/exportDossierCourant.action`)
 
 const fileInput = ref(null)
 const source = 'telepac'
+
+const helpSteps = readonly([
+  { label: 'Je me connecte à Télépac' },
+  { label: `Téléprocédure Dossier PAC ${campagnePacAnnee.value}` },
+  { label: 'Onglet Import/export' },
+  { label: 'Téléchargement du fichier' },
+])
+
+const currentStepIndex = ref(0)
+const nextStepIndex = computed(() => currentStepIndex.value < helpSteps.length ? currentStepIndex.value + 1 : currentStepIndex.value)
 
 async function handleFileUpload () {
   const [archive] = fileInput.value.files
@@ -58,3 +96,9 @@ async function handleFileUpload () {
   emit('upload:complete', { geojson, source })
 }
 </script>
+
+<style scoped>
+img.screenshot {
+  max-width: 650px;
+}
+</style>
