d9378a4139da574b5cd2442595927e12f5f053ff
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-07-05 18:03:42 +0200

    Initial take on Geofolia x OAuth2 flow

--- src/components/OperatorSetup/Geofolia.vue
+++ src/components/OperatorSetup/Geofolia.vue
@@ -5,34 +5,74 @@
             🔐 Connecter mon compte Isagri Geofolia
           </button>
         </article> -->
-    <div class="fr-alert fr-alert--info fr-mb-5w">
-      <p class="fr-alert__title">Remarque</p>
-      <p>Le nom du fichier ressemble à <code>…_Parcelles et Interventions (ZIP)_….zip</code>.</p>
-    </div>
 
-    <div class="fr-upload-group">
-      <input type="file" ref="fileInput" accept=".zip" @change="handleFileUpload" hidden>
+    <ul class="fr-raw-list">
+      <li class="fr-mb-3w">
+        <a :href="VUE_APP_API_ENDPOINT + '/login/geofolia'" class="fr-btn fr-icon-shield-line fr-btn--icon-left">
+          Importer à partir de mon compte Géofolia
+        </a>
+      </li>
 
-      <button class="fr-btn fr-icon-upload-line fr-btn--icon-left" @click="fileInput.click()">
-        Importer mes parcelles et interventions
-      </button>
-    </div>
+      <li>
+        <input type="file" ref="fileInput" accept=".zip" @change="handleFileUpload" hidden>
+        <button class="fr-btn fr-btn--secondary fr-icon-upload-line fr-btn--icon-left" @click="fileInput.click()">
+          Importer à partir d'un fichier parcelles et interventions
+        </button>
+      </li>
+    </ul>
 
+    <hr class="fr-mt-5w fr-mb-3w" />
 
-    <details class="fr-my-5w">
-      <summary class="fr-icon-questionnaire-line fr-btn fr-btn--secondary fr-btn--icon-left">Où trouver mon fichier ?
-      </summary>
+    <fieldset class="fr-fieldset">
+      <legend class="fr-fieldset__legend fr-icon fr-icon-questionnaire-line">
+        Où télécharger le fichier de parcelles et interventions ?
+      </legend>
 
-      <p>
-        Le fichier <b>Parcelles et interventions (ZIP)</b> se trouve dans l'onglet "Export › Parcelles et interventions"
-        du logiciel Géofolia, édité par Isagri.
-      </p>
+      <div class="fr-fieldset__content">
+        <div class="fr-grid-row">
+          <div class="fr-col-5">
+            <ol class="fr-list">
+              <li class="fr-mb-2w">
+                cliquez sur l'icône <b>Export</b> du bandeau de menu
+              </li>
 
-      <p>
-        <img src="/import/geofolia-export.png" class="screenshot"
-          alt="Écran Export Parcelles et interventions du logiciel Géofolia" />
-      </p>
-    </details>
+              <li class="fr-mb-2w">
+                sélectionnez <b>Parcelles et interventions</b>,
+                puis <b>Parcelles et interventions (ZIP)</b>
+              </li>
+
+              <li class="fr-mb-2w">
+                <button class="fr-btn fr-btn--secondary fr-icon-upload-line fr-btn--icon-left fr-mb-2w" @click="fileInput.click()">
+                  Importer mes parcelles et interventions
+                </button>
+                <br />
+
+                <span class="fr-icon fr-icon-info-fill">
+                  le nom du fichier téléchargé ressemble à<br />
+                  <code>…_Parcelles et Interventions (ZIP)_….zip</code>.<br />
+                  Il se trouve dans votre <b>répertoire "Téléchargements"</b>.
+                </span>
+              </li>
+            </ol>
+
+            <p>
+
+            </p>
+          </div>
+
+          <div class="fr-col-7">
+            <p>
+              <img src="/import/geofolia-export.png" class="fr-responsive-img fr-ratio-4x3 screenshot"
+                alt="Écran Export Parcelles et interventions du logiciel Géofolia" />
+            </p>
+          </div>
+        </div>
+
+        <a href="#top" class="fr-mt-5w fr-btn fr-btn--secondary fr-btn--sm fr-btn--icon-left fr-icon-arrow-up-fill">
+          retour en haut de page
+        </a>
+      </div>
+    </fieldset>
   </div>
 </template>
 
