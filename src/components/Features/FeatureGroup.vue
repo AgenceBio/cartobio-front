33bafe085191a5e0bf4a69f56d3d2eb5d3d6aff1
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-09-06 10:33:00 +0200

    Better regroup of the table rows, and select from the map

--- src/components/Features/FeatureGroup.vue
+++ src/components/Features/FeatureGroup.vue
@@ -1,36 +1,32 @@
 <template>
-  <tr @click.stop="open = !open" class="clickable">
-    <td>
-      <div class="fr-checkbox-group single-checkbox">
-        <input type="checkbox" :id="'radio-'+ featureGroup.key" :checked="allSelected" @click="toggleFeatureGroup" />
-        <label class="fr-label" :for="'radio-'+ featureGroup.key" />
-      </div>
-    </td>
-    <th scope="row">
-      <span class="fr-icon fr-icon fr-icon-arrow-right-s-fill" :aria-checked="open" aria-role="button" />
-      {{ featureGroup.label }}
-    </th>
-    <td class="numeric">{{ inHa(featureGroup.surface) }}&nbsp;ha</td>
-  </tr>
-  <tr :hidden="!open">
-    <td colspan="3" class="subtable">
-      <table :aria-label="`Parcelles de ${featureGroup.label}`">
-        <tbody>
-          <tr v-for="feature in featureGroup.features" :key="feature.id" @mouseover="emit('update:hoveredId', feature.id)" @click.prevent.stop="toggleSelectedIds(feature.id)" :aria-current="feature.id === hoveredId ? 'location' : null">
-            <th scope="row" class="rowIdCell">
-              <div class="fr-checkbox-group single-checkbox">
-                <input type="checkbox" :id="'radio-' + feature.id" :checked="selectedIds.includes(feature.id)" @click.stop="toggleSelectedIds(feature.id)" />
-                <label class="fr-label" :for="'radio-' + feature.id" />
-              </div>
-            </th>
-            <td>
-            </td>
-            <td class="numeric">{{ inHa(surface(feature)) }}&nbsp;ha</td>
-          </tr>
-        </tbody>
-      </table>
-    </td>
-  </tr>
+  <tbody>
+    <tr @click.stop="open = !open" class="clickable">
+      <td>
+        <div class="fr-checkbox-group single-checkbox">
+          <input type="checkbox" :id="'radio-'+ featureGroup.key" :checked="allSelected" @click="toggleFeatureGroup" />
+          <label class="fr-label" :for="'radio-'+ featureGroup.key" />
+        </div>
+      </td>
+      <th scope="row">
+        <span class="fr-icon fr-icon fr-icon-arrow-right-s-fill" :aria-checked="open" aria-role="button" />
+        {{ featureGroup.label }}
+      </th>
+      <td class="numeric">{{ inHa(featureGroup.surface) }}&nbsp;ha</td>
+    </tr>
+    <tr class="parcelle" :hidden="!open" v-for="feature in featureGroup.features" :key="feature.id" @mouseover="emit('update:hoveredId', feature.id)" @click.prevent.stop="toggleSelectedIds(feature.id)" :aria-current="feature.id === hoveredId ? 'location' : null">
+      <th scope="row">
+        <div class="fr-checkbox-group single-checkbox">
+          <input type="checkbox" :id="'radio-' + feature.id" :checked="selectedIds.includes(feature.id)" @click.stop="toggleSelectedIds(feature.id)" />
+          <label class="fr-label" :for="'radio-' + feature.id" />
+        </div>
+      </th>
+      <td>
+        <span v-if="feature.properties.NUMERO_I">Ilot {{ feature.properties.NUMERO_I }}<span v-if="feature.properties.NUMERO_P">, parcelle {{ feature.properties.NUMERO_P }}</span></span>
+        <span v-else>{{ feature.properties.NOM }}</span>
+      </td>
+      <td class="numeric">{{ inHa(surface(feature)) }}&nbsp;ha</td>
+    </tr>
+  </tbody>
 </template>
 
 <script setup>
@@ -78,6 +74,24 @@ function toggleFeatureGroup () {
 </script>
 
 <style scoped>
+  .fr-table td.numeric,
+  .fr-table th.numeric {
+    font-variant-numeric: tabular-nums;
+    text-align: right !important;
+  }
+
+  .fr-table tr.parcelle {
+    background-color: var(--background-alt-blue-france);
+  }
+  .fr-table tr.parcelle:nth-child(2n) {
+    background-color: var(--background-default-grey);
+  }
+  .fr-table tr.parcelle:last-child {
+    /* same as .fr-table--bordered tbody tr */
+    background-size: 100% 2px;
+    background-image: linear-gradient(180deg, var(--grey-625-425), var(--grey-625-425));
+  }
+
   .subtable {
     padding: 0;
   }
