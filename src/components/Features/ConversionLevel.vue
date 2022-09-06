4066cea9425846a216d37107787b9ecf364d093b
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-09-06 16:25:22 +0200

    Add new ConversionLevel component

---
+++ src/components/Features/ConversionLevel.vue
@@ -0,0 +1,42 @@
+<template>
+  <i v-if="!feature.properties.conversion_niveau">
+    Donnée inconnue
+  </i>
+  <span class="certification-details" v-else-if="isABLevel(feature.properties.conversion_niveau)">
+    <abbr :title="conversionLevel.label">{{ conversionLevel.shortLabel }}</abbr>
+    <small>engagée le {{ dateDDMMYYY(feature.properties.engagement_date) }}</small>
+  </span>
+  <span v-else>
+    {{ conversionLevel.shortLabel }}
+  </span>
+</template>
+
+<script setup>
+
+import { getConversionLevel, isABLevel } from '@/referentiels/ab.js'
+
+const props = defineProps({
+  feature: {
+    type: Object,
+    required: true
+  }
+})
+
+const conversionLevel = getConversionLevel(props.feature.properties.conversion_niveau)
+
+
+function dateDDMMYYY (date) {
+  return new Date(date).toLocaleDateString('fr-FR', {
+    dateStyle: 'short',
+    timeZone: 'Europe/Paris'
+  })
+}
+</script>
+
+<style scoped>
+  .certification-details {
+    display: flex;
+    justify-content: space-between;
+    /* flex-direction: column; */
+  }
+</style>
