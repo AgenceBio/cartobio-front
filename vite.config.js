ec6464e5995671b9fbd4e26405624f655dfb07e3
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-12-03 00:56:54 -0500

    Ajoute une résolution d'alias Vite pour IntelliSense dans VSCode

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,13 @@ export default defineConfig({
   },
 
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
