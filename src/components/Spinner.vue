9162f939ca2015b31edd2d860e401aef7c1e6f27
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-12-03 00:57:58 -0500

    Crée un composant Spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <p class="spin" aria-label="Chargement en cours…">
+    <slot name="default" />
+  </p>
+</template>
+
+<style scoped>
+@keyframes spinner {
+  0% {
+    transform: translate3d(-50%, -50%, 0) rotate(0deg);
+  }
+  100% {
+    transform: translate3d(-50%, -50%, 0) rotate(360deg);
+  }
+}
+.spin {
+  display: inline-block;
+  height: 40px;
+  padding-left: calc(40px + 1rem);
+  line-height: 40px;
+  position: relative;
+}
+.spin::before {
+  animation: .7s linear infinite spinner;
+  animation-play-state: inherit;
+  border: solid 5px var(--background-alt-grey-hover);
+  border-bottom-color: var(--background-action-high-blue-france);
+  border-radius: 50%;
+  content: "";
+  height: 40px;
+  width: 40px;
+  top: 20px;
+  left: 20px;
+  position: absolute;
+  transform: translate3d(-50%, -50%, 0);
+  will-change: transform;
+}
+</style>
