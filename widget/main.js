8cc4ae477fc3618ee163cabf43b77a85072b4765
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-05-10 00:38:58 +0200

    chore: mise à jour du jeton JWT

--- widget/main.js
+++ widget/main.js
@@ -2,6 +2,9 @@ import { defineCustomElement } from 'vue'
 import { createPinia, setActivePinia } from 'pinia'
 import NotificationCartobio from './Notification.ce.vue'
 
+// Pinia has to be setup in order to use it in subcomponents
+// the following line is a way to setup Pinia without using Vue's `app.use(pinia)`
+// @see https://github.com/vuejs/pinia/discussions/1085#discussioncomment-2851876
 setActivePinia(createPinia())
 
 const NotificationCartobioElement = defineCustomElement({
