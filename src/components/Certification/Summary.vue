f93a099da440ece63ed2f9d9396768c4ad9d14c3
Maud Royer <hello@maudroyer.fr>
2023-06-12 10:50:46 +0200

    fix: double affichage des erreurs dans OperatorSummary et CertificationSummary
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- src/components/Certification/Summary.vue
+++ src/components/Certification/Summary.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="fr-alert fr-alert--warning fr-mb-3w" v-if="hasFailures">
-    <p v-for="([ruleId, result]) in validationRulesWithFailures" :key="ruleId">
-      {{ ruleId === 'NOT_EMPTY' ? `Il manque un type de culture dans ${result.failures} parcelles.` : '' }}
-      {{ ruleId === 'ENGAGEMENT_DATE' ? `Il manque une date d'engagement et/ou un niveau de conversion dans ${result.failures} parcelles.` : '' }}
-    </p>
+    <ValidationErrors :validation-result="validationResult" />
   </div>
 
   <div class="fr-callout fr-callout--blue-ecume" v-else-if="!isAudited && isComplete">
@@ -19,9 +16,10 @@
 
 <script setup>
 import { computed, ref } from 'vue'
-import { applyValidationRules, isCertificationImmutable, CERTIFICATION_STATE } from '@/referentiels/ab.js'
+import { applyValidationRules, CERTIFICATION_STATE, isCertificationImmutable } from '@/referentiels/ab.js'
 import { updateAuditState } from '@/cartobio-api.js'
 import { useRecordStore } from '@/stores/index.js'
+import ValidationErrors from "@/components/Features/ValidationErrors.vue"
 
 const recordStore = useRecordStore()
 
@@ -47,7 +45,6 @@ const props = defineProps({
 const sendOffModal = ref(false)
 const validationResult = computed(() => applyValidationRules(props.validationRules.rules, ...props.features.features))
 const hasFailures = computed(() => Boolean(validationResult.value.failures))
-const validationRulesWithFailures = computed(() => Object.entries(validationResult.value.rules).filter(([, { failures }]) => failures))
 const isComplete = computed(() => hasFailures.value === false)
 const isAudited = computed(() => isCertificationImmutable(props.record.certification_state))
 
