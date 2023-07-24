52be4f9663875c8601a055ed76466c6380a0f446
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-07-24 16:23:57 +0200

    fix(parcelles): utilise les helpers de permission pour déterminer si on cause à un·e OC

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,3 +1,4 @@
 export { useFeaturesStore } from './features.js'
+export { usePermissions } from './permissions.js'
 export { useRecordStore } from './record.js'
 export { useUserStore } from './user.js'
