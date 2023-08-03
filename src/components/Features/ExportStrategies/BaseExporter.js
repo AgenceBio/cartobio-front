607c3177af57bf6177ba99c36d801d95aeae4d95
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-08-03 17:11:00 +0200

    fix(export): ajoute davantage de précisions en commentaire
    Le numéro ilot.parcelle est optionnel

--- src/components/Features/ExportStrategies/BaseExporter.js
+++ src/components/Features/ExportStrategies/BaseExporter.js
@@ -1,3 +1,5 @@
+import { cultureLabel, featureName } from "../index.js"
+
 export default class BaseExporter {
   label = ''
   extension = ''
@@ -7,4 +9,23 @@ export default class BaseExporter {
     this.featureCollection = featureCollection
     this.operator = operator
   }
+}
+
+/**
+ *
+ * @param {Feature[]} features
+ * @returns {String}
+ */
+export function generateAutresInfos (features, { withName= true } = {}) {
+  return features.map(feature => {
+    const name = withName ? featureName(feature, { ilotLabel: '', parcelleLabel: '', separator: '.', placeholder: '' }) : ''
+
+    const cultures = feature.properties.cultures.map(c => {
+      return [cultureLabel(c, { withCode: true }), c.variete, c.date_semis, c.surface ? `${c.surface}ha` : ''].filter(d => d).join(', ')
+    }).join(' / ')
+
+    return [name, cultures, feature.properties.auditeur_notes].filter(d => d).join(', ')
+  })
+  .filter(d => d)
+  .join(' ; ')
 }
