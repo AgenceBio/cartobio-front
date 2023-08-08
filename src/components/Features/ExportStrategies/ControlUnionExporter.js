bebe7c46790f0bd0ca5ab7d4de75d7f6160ff161
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-08-08 11:34:03 +0200

    fix(export): ajoute deux colonnes à l'export ControlUnion

--- src/components/Features/ExportStrategies/ControlUnionExporter.js
+++ src/components/Features/ExportStrategies/ControlUnionExporter.js
@@ -1,30 +1,47 @@
-import BaseExporter from "@/components/Features/ExportStrategies/BaseExporter.js";
+import BaseExporter, { generateAutresInfos } from "@/components/Features/ExportStrategies/BaseExporter.js";
 import { utils, write } from "xlsx";
 import { surface } from "@/components/Features/index.js"
 import { fromCodeCpf } from "@agencebio/rosetta-cultures"
 
 const getSheet = ({ featureCollection, operator }) => {
-  const notification = operator.notifications.find(({ status }) => status === 'ACTIVE') ?? operator.notifications.at(0)
-
   // First sheet
   // First sheet: customer informations (via `customer`)
   const sheet = utils.aoa_to_sheet([
-    ['Raison sociale :', notification.name ?? '', 'Date de l\'audit :', ''],
+    ['Raison sociale :', operator.nom, 'Date de l\'audit :', ''],
   ])
 
   utils.sheet_add_aoa(sheet, [
     [],
     ['Suivi parcelles'],
     [],
-    ['N° PACAGE :'],
+    ['N° PACAGE :', operator.numeroPacage ?? ''],
     [
       'Identification (références cadastrales)',
       'Production',
       'Quantité',
       'Date de début de conversion',
-      'Niveau de la parcelle au jour de l\'audit (C1'
+      'Niveau de la parcelle au jour de l\'audit (C1/C2/C3/AB)',
+      'Autres infos',
+      'Id. CartoBio'
     ],
   ], { origin: 'A2' })
+
+  sheet['!cols'] = [
+    // ref cadastrale
+    { wch: 16 },
+    // Production
+    { wch: 40 },
+    // Surface / Quantité
+    { wch: 16 },
+    // Date de début de conversion
+    { wch: 12 },
+    // Niveau de conversion
+    { wch: 8 },
+    // Autres infos,
+    { wch: 40 },
+    // Id. CartoBio
+    { wch: 24 }
+  ]
 
   sheet['!merges'] = [
     utils.decode_range('A3:E3')
@@ -35,13 +52,20 @@ const getSheet = ({ featureCollection, operator }) => {
     const culture = fromCodeCpf(props.cultures.at(0)?.CPF)
 
     return [
-      '',
+      props.cadastre,
       culture?.libelle_code_cpf,
       surfaceHa,
       props.engagement_date ? new Date(props.engagement_date) : '',
-      props.niveau_conversion
+      props.niveau_conversion,
+      generateAutresInfos([{ properties: props }], { initialCulture: culture?.code_cpf }),
+      props.id
     ]
   }), { origin: 'A7' })
+
+  // Formattage des cellules
+  featureCollection.features.forEach((feature, index) => {
+    sheet[`G${7 + index}`].t = 's';
+  })
 
   return sheet
 }
