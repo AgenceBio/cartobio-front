c1cadc6a3e1e14f4cc596627cdec7cbd13a30688
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-08-09 14:54:38 +0200

    feat(export): Export Qualisud

--- src/components/Features/ExportStrategies/index.js
+++ src/components/Features/ExportStrategies/index.js
@@ -1,20 +1,25 @@
 import DefaultExporter from './DefaultExporter.js'
-import CertipaqExporter from './CertipaqExporter.js'
 import BureauVeritasExporter from './BureauVeritasExporter.js'
-import OcaciaExporter from "@/components/Features/ExportStrategies/OcaciaExporter.js";
+import CertipaqExporter from './CertipaqExporter.js'
 import ControlUnionExporter from "@/components/Features/ExportStrategies/ControlUnionExporter.js"
+import OcaciaExporter from "@/components/Features/ExportStrategies/OcaciaExporter.js";
+import QualisudExporter from "@/components/Features/ExportStrategies/QualisudExporter.js";
 
 export default DefaultExporter
-export { CertipaqExporter, BureauVeritasExporter, OcaciaExporter, ControlUnionExporter }
+export { BureauVeritasExporter, CertipaqExporter, ControlUnionExporter, OcaciaExporter, QualisudExporter }
 
 const isProduction = Boolean(import.meta.env.PROD)
 
 const exporters = new Map([
   // Use a custom exporter in development to ease testing
-  [1, isProduction ? DefaultExporter : CertipaqExporter],
+  [1, isProduction ? DefaultExporter : QualisudExporter],
   // Production exports
   [2, BureauVeritasExporter],
   [3, CertipaqExporter],
+  [4, QualisudExporter],
+  // [5, CertisudExporter],
+  // [6, CertisExporter],
+  // [7, BureauAlpesControlesExporter],
   [10, ControlUnionExporter],
   [11, OcaciaExporter],
 ])
