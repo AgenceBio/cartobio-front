54cb6f8cec6ae6bc3774c2e8bd5943fab14e2596
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-10-05 15:49:17 +0200

    feat(Accordion): ajout d'un composant accordéon
    Une mise à jour du DSFR est nécessaire pour qu'il soit stylé correctement dans son état actif.

---
+++ src/components/DesignSystem/Accordion.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="fr-accordion">
+    <h3 class="fr-accordion__title">
+      <button class="fr-accordion__btn" :aria-expanded="isOpen" @click="isOpen = !isOpen" :aria-controls="elementId" type="button">
+        {{ title }}
+      </button>
+    </h3>
+
+    <div :class="{ 'fr-collapse': true, 'fr-collapse--expanded': isOpen }" :id="elementId">
+      <slot name="default" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+const elementId = ref(`accordion-${crypto.randomUUID()}`)
+const isOpen = ref(props.open)
+</script>
+
+<style scoped>
+.fr-collapse {
+  --collapse-max-height: none;
+}
+</style>
