434d95f4cc0a8c53f735f4269c78778528bbd51c
Thomas Parisot <thom4parisot@users.noreply.github.com>
2024-03-05 19:52:46 +0100

    feat(map): affiche les noms de parcelles par défaut

--- src/components/Map/FeaturesLayer.vue
+++ src/components/Map/FeaturesLayer.vue
@@ -4,7 +4,7 @@
 <script>
 import GeojsonLayer from "@/components/Map/GeojsonLayer.vue"
 import { useFeaturesStore } from "@/stores/index.js"
-import { inject, watch } from "vue"
+import { inject } from "vue"
 
 export default {
   extends: GeojsonLayer,
@@ -24,13 +24,9 @@ export default {
       type: Boolean,
       default: false
     },
-    showNumerosIlots: {
-      type: Boolean,
-      default: false
-    },
     style: {
       type: Object,
-      default: (props) => ({
+      default: () => ({
         layers: [
           {
             "id": "numeros-ilots",
@@ -48,7 +44,7 @@ export default {
               ],
               "text-font": ["Noto Sans Regular"],
               "text-size": 12,
-              "visibility": props.showNumeroIlots ? "visible" : "none",
+              "visibility": "visible",
             },
             "paint": {
               "text-color": "rgba(0, 0, 0, 1)",
@@ -64,17 +60,6 @@ export default {
   setup(props) {
     const featureStore = useFeaturesStore()
     const map = inject('map')
-
-    watch(() => props.showNumerosIlots, (showNumeroIlots) => {
-      const visibility = showNumeroIlots ? 'visible' : 'none'
-
-      if (map.value.isStyleLoaded()) {
-        map.value.setLayoutProperty('parcellaire-operateur/numeros-ilots', 'visibility', visibility)
-      }
-      else {
-        map.value.once('load', () => map.value.setLayoutProperty('parcellaire-operateur/numeros-ilots', 'visibility', visibility))
-      }
-    }, { immediate: true })
 
     if (props.interactive) {
       featureStore.bindMaplibreFeatureState(map.value, 'parcellaire-operateur/data')
