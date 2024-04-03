04af5f2020f7ac966b20f5079fac9fbf08eac4d5
Thomas Parisot <thom4parisot@users.noreply.github.com>
2024-04-03 16:58:43 +0200

    fix(telepac): affiche les erreurs personnalisées plutôt qu'une générique

--- src/components/OperatorSetup/Sources/Telepac.vue
+++ src/components/OperatorSetup/Sources/Telepac.vue
@@ -55,7 +55,7 @@ async function handleFileUpload () {
     emit('upload:complete', { geojson, source, warnings, metadata })
   } catch (error) {
     if (error.response?.status >= 400 && error.response?.status < 500) {
-      erreur.value = 'Votre fichier ne semble pas être une déclaration PAC valide.'
+      erreur.value = error.response.data.error
     } else {
       erreur.value = 'Erreur inconnue, merci de réessayer plus tard.'
       throw error
