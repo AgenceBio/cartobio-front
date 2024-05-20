687086dffe43577648732c4e75b162599a40ca5e
Maud Royer <hello@maudroyer.fr>
2024-05-20 17:45:18 +0200

    fix: utiliser le store storage dans la liste des versions
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

---
+++ src/components/record/modals/DeleteDownloadModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <Modal @close="emit('close')" data-track-content data-content-name="Modale de suppression d'un téléchargement de parcellaire">
+    <template #title>Supprimer des téléchargements</template>
+
+    <p>
+      Cette version du parcellaire ne sera plus disponible hors connexion
+    </p>
+
+    <template #footer>
+      <ul class="fr-btns-group fr-btns-group--inline-lg">
+        <li>
+          <button class="fr-btn" @click="handleDelete">
+            Supprimer
+          </button>
+        </li>
+        <li>
+          <button class="fr-btn fr-btn--secondary" @click="$emit('close')">
+            Annuler
+          </button>
+        </li>
+      </ul>
+    </template>
+  </Modal>
+</template>
+
+<script setup>
+import Modal from "@/components/Modal.vue"
+import { useCartoBioStorage } from "@/stores/storage.js"
+
+const props = defineProps({
+  recordId: {
+    type: String,
+    required: true
+  },
+})
+const emit = defineEmits(['close'])
+
+const storage = useCartoBioStorage()
+
+async function handleDelete() {
+  storage.clearRecord(props.recordId)
+  emit('close')
+}
+</script>
