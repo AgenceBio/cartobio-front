2c80ba7070a69f3c4993423b3226ab5f715affd7
Hugoobx <hugo.bertoux@icloud.com>
2025-05-06 14:11:55 +0200

    fix: fix de la pop-in en cas de conflit à gérer

--- src/components/versions/VersionConflictModal.vue
+++ src/components/versions/VersionConflictModal.vue
@@ -11,9 +11,12 @@ const props = defineProps({
 });
 
 const storage = useCartoBioStorage();
+const isNewVersionLoading = ref(false);
 
 async function duplicate() {
+  isNewVersionLoading.value = true;
   await storage.resolveConflict(props.recordId, true);
+  isNewVersionLoading.value = false;
   emit("close");
 }
 
@@ -27,8 +30,8 @@ async function merge() {
   <Modal @close="$emit('close')" v-bind="$attrs" icon="fr-icon-warning-fill">
     <template #title>Conflit entre versions</template>
     <p>
-      Il semblerait qu’une autre personne ait effectué des modifications sur un parcellaire pendant que vous le
-      modifiiez hors-ligne :
+      Il semblerait qu’une autre personne ait effectué des modifications sur un parcellaire pendant que vous le modifiez
+      hors-ligne :
     </p>
     <p>
       Version : <b>{{ storage.records[recordId].version_name }}</b> de l’exploitation
@@ -40,13 +43,19 @@ async function merge() {
     <template #footer>
       <ul class="fr-btns-group fr-btns-group--inline">
         <li>
-          <button class="fr-btn" @click="duplicate">Créer une nouvelle version</button>
+          <button class="fr-btn" @click="duplicate">
+            <div v-if="isNewVersionLoading">
+              <Spinner :hint="'Cela peut prendre quelques secondes...'"> Création en cours... </Spinner>
+            </div>
+            <span> <p>Créer une nouvelle version</p> </span>
+          </button>
         </li>
         <li>
           <button
             class="fr-btn fr-btn--tertiary"
             @click="merge"
             aria-label="Appliquer les changements sur la version existante"
+            :disabled="storage.records[recordId].certification_state === 'CERTIFIED'"
           >
             Appliquer les changements
           </button>
