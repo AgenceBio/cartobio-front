818aa505b9c9350ce4cbebe95759dc05b43a86d8
Hugoobx <hugo.bertoux@icloud.com>
2025-09-30 14:22:54 +0200

    feat: ✨ nouvelle modal ajout de nouvelle version

--- src/components/versions/NewVersionModal.vue
+++ src/components/versions/NewVersionModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useFocus } from "@vueuse/core";
 import Modal from "@/components/widgets/Modal.vue";
 import { useOperatorStore } from "@/stores/operator.js";
@@ -7,12 +7,21 @@ import { useRecordStore } from "@/stores/record.js";
 import { sources } from "@/referentiels/imports.js";
 import { createOperatorRecord } from "@/cartobio-api.js";
 import { useRouter } from "vue-router";
+import { jjmmyyyy } from "@/utils/dates";
+import { inHa } from "@/utils/features.js";
+import State from "@/components/records/State.vue";
+import toast from "@/utils/toast.js";
 
 const router = useRouter();
 const operatorStore = useOperatorStore();
 const recordStore = useRecordStore();
 const autofocusedElement = ref();
+const selectedOption = ref(null);
+const selectedRecord = ref(null);
+
 useFocus(autofocusedElement, { initialValue: true });
+
+const sortedRecords = computed(() => operatorStore.records);
 
 async function createEmptyVersion() {
   recordStore.update(
@@ -28,70 +37,142 @@ async function createEmptyVersion() {
 
   await router.push(`/exploitations/${operatorStore.operator.numeroBio}/${recordStore.record.record_id}`);
 }
+
+const handleSubmit = async () => {
+  if (selectedOption.value === "import") {
+    await router.push(`${operatorStore.operator.numeroBio}/import`);
+  } else if (selectedOption.value === "empty") {
+    await createEmptyVersion();
+  } else if (selectedOption.value === "duplicate" && selectedRecord.value) {
+    const result = await recordStore.duplicate(selectedRecord.value.record_id);
+    if (result) toast.success("La version a été dupliquée");
+    emit("close");
+  }
+};
 </script>
 
 <template>
-  <Modal @close="$emit('close')" v-bind="$attrs" icon="fr-icon-add-line">
+  <Modal large @close="$emit('close')" v-bind="$attrs" icon="fr-icon-add-line">
     <template #title>Créer une nouvelle version</template>
     <p>Il existe plusieurs façons de créer une nouvelle version du parcellaire.</p>
 
-    <div class="fr-card fr-enlarge-link fr-mb-2w">
-      <div class="fr-card__body">
-        <div class="fr-card__content">
-          <h3 class="fr-card__title">
-            <router-link
-              :to="`/exploitations/${operatorStore.operator.numeroBio}/import`"
-              class="fr-card__link"
-              ref="autofocusedElement"
-            >
-              Importer un parcellaire informatisé
-            </router-link>
-          </h3>
-          <p class="fr-card__desc">
-            Importez en un clic votre parcellaire informatisé via votre déclaration <b>PAC</b>, le dossier <b>nCVI</b>,
-            <b>MesParcelles</b> ou <b>Geofolia</b>.
-          </p>
+    <fieldset class="fr-fieldset" aria-labelledby="radio-versions">
+      <legend class="fr-fieldset__legend--regular fr-fieldset__legend" id="radio-versions-legend">
+        Choisissez un mode de création
+      </legend>
+      <div class="fr-fieldset__element">
+        <div class="fr-radio-group">
+          <input type="radio" id="import-version" value="import" v-model="selectedOption" ref="autofocusedElement" />
+          <label class="fr-label" for="import-version">
+            Importer un parcellaire informatisé
+            <span class="fr-hint-text">
+              Importez via votre déclaration <b>PAC</b>, le dossier <b>nCVI</b>, <b>MesParcelles</b> ou <b>Geofolia</b>.
+            </span>
+          </label>
         </div>
       </div>
-    </div>
-    <div class="fr-card fr-enlarge-link fr-mb-2w">
-      <div class="fr-card__body">
-        <div class="fr-card__content">
-          <h3 class="fr-card__title">
-            <a
-              :href="`/exploitations/${operatorStore.operator.numeroBio}/nouveau`"
-              @click.prevent="createEmptyVersion"
-              class="fr-card__link"
-            >
-              Créer à partir de zéro
-            </a>
-          </h3>
-          <p class="fr-card__desc">
-            Ajouter vos parcelles une à une, par référence cadastrale ou dessin sur la carte, sans repartir d’un
-            parcellaire existant.
-          </p>
+
+      <div class="fr-fieldset__element">
+        <div class="fr-radio-group">
+          <input type="radio" id="create-empty" value="empty" v-model="selectedOption" />
+          <label class="fr-label" for="create-empty">
+            Créer à partir de zéro
+            <span class="fr-hint-text">
+              Ajouter vos parcelles une à une, par référence cadastrale ou dessin sur la carte.
+            </span>
+          </label>
         </div>
       </div>
-    </div>
-    <div class="fr-card">
-      <div class="fr-card__body">
-        <div class="fr-card__content">
-          <h3 class="fr-card__title">Dupliquer une autre version</h3>
-          <p class="fr-card__desc">
-            Vous pouvez également dupliquer une version existante depuis l’écran précédent, en ouvrant le menu des
-            actions de la version souhaitée.<br />
-            <a
-              href="https://docs-cartobio.agencebio.org/agriculteurs.trices/pas-a-pas/gestion-des-versions-de-parcellaire#dupliquer-un-parcellaire"
-              target="_blank"
-              rel="noopener"
-            >
-              Voir l'aide <span class="fr-icon-arrow-right-line fr-icon--sm" aria-hidden="true"></span><lien-externe />
-            </a>
-          </p>
+      <div class="fr-fieldset__element">
+        <div class="fr-radio-group">
+          <input type="radio" id="duplicate" value="duplicate" v-model="selectedOption" />
+          <label class="fr-label" for="duplicate">
+            Dupliquer une autre version
+            <span class="fr-hint-text"> Veuillez sélectionner la version à dupliquer </span>
+          </label>
         </div>
       </div>
+    </fieldset>
+    <div
+      class="fr-table table-data fr-table--bordered version-table fr-table--no-caption"
+      v-if="selectedOption == 'duplicate'"
+    >
+      <table aria-hidden="true" aria-describedby="versions-summary-global">
+        <colgroup>
+          <col class="blank-column" />
+          <col class="version-name" />
+          <col class="audit-date" />
+          <col class="surface" />
+          <col class="parcelles" />
+          <col class="statut" />
+        </colgroup>
+        <thead>
+          <tr class="column-headers">
+            <th>Nom de version</th>
+            <th>Date d'audit</th>
+            <th>Surface</th>
+            <th>Parcelles</th>
+            <th>Statut</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="record in sortedRecords" :key="record.id" @click="selectedRecord = record">
+            <td class="version-name">
+              <div class="fr-radio-group fr-radio-group--sm">
+                <input
+                  type="radio"
+                  class="radio-button"
+                  id="radio-{{record.id}}"
+                  name="radio-inline"
+                  v-model="selectedRecord"
+                  :value="record"
+                />
+                <label class="fr-label" for="radio-{{record.id}}"> {{ record.version_name }} </label>
+              </div>
+            </td>
+            <td class="audit-date">{{ record.audit_date ? jjmmyyyy(record.audit_date) : "Non audité" }}</td>
+            <td class="surface">{{ inHa(record.surface) }} ha</td>
+            <td class="parcelles">{{ record.parcelles }}</td>
+            <td class="statut">
+              <State :record="record" :show-date="false" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    <template #footer>
+      <div class="footer-row">
+        <div class="btn-container">
+          <button
+            class="fr-btn"
+            @click="handleSubmit"
+            :disabled="!selectedOption || (selectedOption === 'duplicate' && !selectedRecord)"
+          >
+            Valider
+          </button>
+        </div>
+      </div>
+    </template>
   </Modal>
 </template>
 
-<style scoped></style>
+<style scoped>
+.space-between {
+  display: flex;
+  justify-content: space-between !important;
+  align-items: center;
+}
+tr:hover {
+  background-color: var(--background-alt-blue-france-hover) !important;
+}
+
+.footer-row {
+  display: flex;
+  align-items: center;
+}
+
+.btn-container {
+  margin-left: auto;
+  margin-right: 0;
+}
+</style>
