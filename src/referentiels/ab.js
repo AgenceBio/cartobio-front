41510092f0546272cae896753bfc73071ac732fa
Hugoobx <hugo.bertoux@icloud.com>
2025-10-06 14:26:27 +0200

    fix: 🐛prettier

--- src/referentiels/ab.js
+++ src/referentiels/ab.js
@@ -299,11 +299,13 @@ export const notificationsStateLevel = {
 export const engagementList = {
   ["AB"]: {
     label: "100% AB",
-    tooltipText: "En 100% AB, toutes les parcelles de l'exploitation ont atteint le niveau AB (hors bande tampon, fleurie et bordure)",
+    tooltipText:
+      "En 100% AB, toutes les parcelles de l'exploitation ont atteint le niveau AB (hors bande tampon, fleurie et bordure)",
   },
   ["ABCONV"]: {
     label: "AB/en conversion",
-    tooltipText: "Présence sur l’exploitation de parcelles en conversion sans parcelle en conventionnel (hors bande tampon, fleurie et bordure)",
+    tooltipText:
+      "Présence sur l’exploitation de parcelles en conversion sans parcelle en conventionnel (hors bande tampon, fleurie et bordure)",
   },
   ["MIXTE"]: {
     label: "Mixte",
